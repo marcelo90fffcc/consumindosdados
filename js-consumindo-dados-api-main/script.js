b290e6959fabf8b7afd1e7f07057919a72ed2582
marcelo90fffcc <129770947+marcelo90fffcc@users.noreply.github.com>
2023-10-30 11:37:33 -0300

    Add files via upload

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,6 +1,26 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-  .then(resposta => resposta.json())
-  .then(r => console.log(r));
-  .catch(erro => console.log(erro));
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+  try {
+      var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      var consultaCEPConvertida = await consultaCEP.json();
+      if (consultaCEPConvertida.erro) {
+          throw Error('CEP não existente!');
+      }
+      var cidade = document.getElementById('cidade');
+      var logradouro = document.getElementById('endereco');
+      var estado = document.getElementById('estado');
 
-console.log(consultaCEP);
+      cidade.value=consultaCEPConvertida.localidade;
+      logradouro.value=consultaCEPConvertida.logradouro;
+      estado.value=consultaCEPConvertida.uf;
+
+      console.log(consultaCEPConvertida);
+      return consultaCEPConvertida;
+  } catch (erro) {
+      console.log(erro);
+  }
+}
+
+  var cep = document.getElementById('cep');
+  cep.addEventListener("focusout", () => buscaEndereco(cep.value));
